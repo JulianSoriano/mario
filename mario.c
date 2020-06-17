36da2e75c5b0377d220518416edeae7a953647ac
Julian Soriano <jsoriano.re@gmail.com>
2020-06-17 15:32:54 -0400

    Update mario.c
    Now the more difficult version of the assignment.

--- mario.c
+++ mario.c
@@ -1,45 +1,65 @@
 //pull data from these libraries
 #include <cs50.h>
 #include <stdio.h>
+////////////////////////////////
+
+//Variables:
+int height;
+
+////////////////////////////////
 
 //main body of code
 int main(void)
 {
-    //prompt for height that only accepts values using 1 to 8:
-
-    //variables
-    int input;
+    //prompt user for height between 1 and 8:
 
     //do the following and then check for while condition:
     do
     {
         //prompt user for "Height: "
-        input = get_int("Height: ");
+        height = get_int("Height: ");
     }
-    //if input is smaller than 1 or greater than 8, re-prompt user
-    while (input < 1 || input > 8);
+
+    //if height is smaller than 1 or greater than 8, re-prompt user
+    while (height < 1 || height > 8);
+
+    //////////////////////////////////////////////////////////////
 
     //pyramid:
 
-    //height starts at 1, if smaller or equal to input, add 1 and repeat until height matches input
-    for (int height = 1; height <= input; height++)
+    //i represents the current row
+    for (int i = 1; i <= height; i++)
     {
-        //space starts at 0, if space is smaller than (input minus height), add a space
-        for (int space = 0; space < (input - height); space++)
+        //gap to the left of the pyramids:
+        for (int space = 0; space < (height - i); space++)
         {
-            //output space
             printf(" ");
         }
 
-        //width starts at 0, if smaller than height, add 1 and repeat until width matches input
-        for (int width = 0; width < height; width++)
+        ///////////////////////////////////////////////////////////
+
+        //left pyramid:
+        for (int leftbrick = 0; leftbrick < i; leftbrick++)
         {
-            //output brick
             printf("#");
         }
 
-//drop prompt to new line
+        ///////////////////////////////////////////////////////////
+
+        //gap between the pyramids
+        printf("  ");
+
+        ///////////////////////////////////////////////////////////
+
+        //right pyramid:
+        for (int rightbrick = 0; rightbrick < i; rightbrick++)
+        {
+            printf("#");
+        }
+
+        ///////////////////////////////////////////////////////////
+
+        //prompt to a new line
         printf("\n");
     }
-
 }
